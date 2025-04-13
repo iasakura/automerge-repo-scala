abfc0f3241d06dc2f3cab201589777cbb316597e
iasakura <5370686+iasakura@users.noreply.github.com>
2025-04-14 00:20:19 +0900

    example

--- build.sbt
+++ build.sbt
@@ -4,14 +4,16 @@ val toolkitTest = "org.scala-lang" %% "toolkit-test" % toolkitV
 
 val PekkoVersion = "1.1.3"
 libraryDependencies += ("org.apache.pekko" %% "pekko-actor-typed" % PekkoVersion)
-  .cross(
-    CrossVersion.for3Use2_13
-  )
+libraryDependencies += ("org.apache.pekko" %% "pekko-actor-testkit-typed" % PekkoVersion % Test)
 
 ThisBuild / scalaVersion := "3.3.4"
 libraryDependencies += toolkit
 libraryDependencies += (toolkitTest % Test)
 libraryDependencies += "com.lihaoyi" %% "cask" % "0.9.7"
 libraryDependencies += ("org.automerge" % "automerge" % "0.0.7")
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.19"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % Test
+
+addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "2.0.8")
 
 fork := true
--- src/test/scala/example/ExampleSuite.scala
+++ src/test/scala/example/ExampleSuite.scala
@@ -1,8 +1,42 @@
 package example
 
+import org.automerge.Document
+import scala.util.Using
+import org.automerge.ObjectId
+import org.automerge.ObjectType
+
 class ExampleSuite extends munit.FunSuite:
 
   test("addition") {
     assert(1 + 1 == 2)
+  }
+
+  test("automerge works!") {
+    val doc = Document()
+    val text = Using(doc.startTransaction()) { tx =>
+      val text = tx.set(ObjectId.ROOT, "text", ObjectType.TEXT)
+      tx.spliceText(text, 0, 0, "Hello world");
+      tx.commit()
+      text
+    }.get
+
+    val docBytes = doc.save()
+
+    val doc2 = Document.load(docBytes)
+    println(doc2.text(text).get())
+
+    Using(doc2.startTransaction()) { tx =>
+      tx.spliceText(text, 5, 0, "beautiful")
+      tx.commit()
+    }
+
+    Using(doc.startTransaction()) { tx =>
+      tx.spliceText(text, 5, 0, " there")
+      tx.commit()
+    }
+
+    doc.merge(doc2)
+
+    println(doc.text(text).get())
   }
 end ExampleSuite
